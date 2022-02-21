c644486c4260df26c3e928113650460570ec5e5c
taisei12232 <taisei12232@gmail.com>
2022-02-21 20:57:07 +0900

    fix: change keys

--- src/component/firebase.js
+++ src/component/firebase.js
@@ -2,10 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.APIKEY,
-    authDomain: process.env.AUTHDOMAIN,
-    projectId: process.env.PROJECTID,
-    databaseURL: process.env.DATABASEURL
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    databaseURL: process.env.REACT_APP_DATABASEURL
 }
 
 initializeApp(firebaseConfig);
