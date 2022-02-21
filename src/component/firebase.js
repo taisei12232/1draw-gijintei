d5dcee36798250466e1b2a78207d90756f9e6cb7
taisei12232 <taisei12232@gmail.com>
2022-02-21 20:48:57 +0900

    update: add urls

--- src/component/firebase.js
+++ src/component/firebase.js
@@ -4,8 +4,10 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_APIKEY,
     authDomain: process.env.REACT_APP_AUTHDOMAIN,
-    projectId: process.env.REACT_APP_PROJICTID
+    projectId: process.env.REACT_APP_PROJICTID,
+    databaseURL: process.env.REACT_APP_DATABASEURL
 }
+
 initializeApp(firebaseConfig);
 const db = getFirestore();
 
