3715bad7d0f0261243da63d037ff367227ec4896
taisei12232 <79786745+taisei12232@users.noreply.github.com>
2022-02-25 07:52:09 +0900

    Update Days.js

--- src/component/Days.js
+++ src/component/Days.js
@@ -1,7 +1,7 @@
 const dt = new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000));
 
 var days = [];
-for(var i = 0;i < 30;i++){
+for(var i = 0;i < 40;i++){
     days.push(dt.getFullYear() + "-" + (dt.getMonth()+1) + "-" +  dt.getDate());
     dt.setDate(dt.getDate() - 1);
 }
