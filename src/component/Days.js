72537a526337b345c297f2b65f51ef62c0aa9461
taisei12232 <taisei12232@gmail.com>
2022-02-27 21:19:00 +0900

    refactor: add day break

--- src/component/Days.js
+++ src/component/Days.js
@@ -1,9 +1,12 @@
 const dt = new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000));
 
 var days = [];
-for(var i = 0;i < 30;i++){
-    days.push(dt.getFullYear() + "-" + (dt.getMonth()+1) + "-" +  dt.getDate());
+var day = "";
+while(1){
+    day = dt.getFullYear() + "-" + (dt.getMonth()+1) + "-" +  dt.getDate();
+    days.push(day);
     dt.setDate(dt.getDate() - 1);
+    if(day == "2022-1-17") break;
 }
 
 export {days};
