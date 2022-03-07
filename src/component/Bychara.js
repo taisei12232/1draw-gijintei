09fd8d92be61c28ec1fe313ac141958d4780d416
taisei12232 <taisei12232@gmail.com>
2022-03-07 19:18:40 +0900

    feat: initialize character page

--- src/component/Bychara.js
+++ src/component/Bychara.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import './Bychara.css';
 
 function About() {
     const { name } = useParams();
     return (
-        <div>
+        <div className='component-bychara'>
             <h1>{name}</h1>
             <h2>description</h2>
             <p>蓬莱姉妹の長女。身長が姉妹の中で1番小さいがこれでも中央都市区のエリアAの管理者。妹達の支え（マスコット）となっている。</p>
