4a5f784cf6c58aa13416137997acb4975a9d089e
taisei12232 <taisei12232@gmail.com>
2022-03-08 16:43:53 +0900

    update: add new routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './component/Header';
 import Tweets from './component/Tweets';
 import Bychara from './component/Bychara';
+import Farday from './component/Farday';
+import Notfound from './component/Notfound';
 
 import {
   BrowserRouter as Router,
@@ -19,8 +21,9 @@ function App() {
         <Routes>
           <Route path="/" />
           <Route path="/1draw" element={<Tweets />} />
-          <Route path="/bychara" element={<Bychara />} />
+          <Route path="/farday" element={<Farday />} />
           <Route path="/bychara/:name" element={<Bychara />} />
+          <Route path='/*' element={<Notfound />}
         </Routes>
       </Router>
     </div>
