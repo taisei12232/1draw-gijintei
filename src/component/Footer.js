a77a88e899e139dae5c3bf828dbdb89cb64fa40c
taisei12232 <taisei12232@gmail.com>
2022-03-31 20:26:56 +0900

    style: change use span

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -5,7 +5,7 @@ function Footer(){
     return (
         <footer className="component-footer">
             <a href="https://twitter.com/hashtag/桜花擬人亭版深夜の真剣お絵描き60分一本勝負">#桜花擬人亭版深夜の真剣お絵描き60分一本勝負</a>
-            <p>Copyright ©</p><a href="https://twitter.com/RAI102ing__Au">アダザクラ【ADA✿ZAKURA】</a><p> All Rights Reserved.</p>
+            <span>Copyright © </span><a href="https://twitter.com/RAI102ing__Au">アダザクラ【ADA✿ZAKURA】</a><span> All Rights Reserved.</span>
         </footer>
     );
 }
